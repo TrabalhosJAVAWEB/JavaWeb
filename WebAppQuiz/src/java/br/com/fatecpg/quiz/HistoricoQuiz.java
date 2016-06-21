115446794e47c102d00c602cea2feda927e8dd9d
David Souza dos Santos <david.xvc93@gmail.com>
2016-06-21 00:38:46 -0300

    Criação de classes

---
+++ WebAppQuiz/src/java/br/com/fatecpg/quiz/HistoricoQuiz.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fatecpg.quiz;
+
+import java.util.Date;
+
+/**
+ *
+ * @author david_000
+ */
+public class HistoricoQuiz {
+    private int codigo;
+    private String login;
+    private double pontuacao;
+    private Date data;
+
+    public HistoricoQuiz(int codigo, String login, double pontuacao, Date data) {
+        this.codigo = codigo;
+        this.login = login;
+        this.pontuacao = pontuacao;
+        this.data = data;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public double getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(double pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+
+    
+    
+}
--- WebAppQuiz/src/java/br/com/fatecpg/quiz/Usuario.java
+++ WebAppQuiz/src/java/br/com/fatecpg/quiz/Usuario.java
@@ -7,4 +7,43 @@ public class Usuario {
     private String nome;
     private long hash;
     private boolean administrador;
+
+    public Usuario(String Login, String nome, long hash, boolean administrador) {
+        this.Login = Login;
+        this.nome = nome;
+        this.hash = hash;
+        this.administrador = administrador;
+    }
+
+    public String getLogin() {
+        return Login;
+    }
+
+    public void setLogin(String Login) {
+        this.Login = Login;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getHash() {
+        return hash;
+    }
+
+    public void setHash(long hash) {
+        this.hash = hash;
+    }
+
+    public boolean isAdministrador() {
+        return administrador;
+    }
+
+    public void setAdministrador(boolean administrador) {
+        this.administrador = administrador;
+    }
 }
